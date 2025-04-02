32e64abf2eead73a6a9afd072ff4772e43ef5d20
Branches <marcus.branches@icloud.com>
2025-04-02 19:41:53 -0300

    remove: removendo mappers desnecessários

--- src/main/java/com/branches/mapper/ClientMapper.java
+++ src/main/java/com/branches/mapper/ClientMapper.java
@@ -12,7 +12,5 @@ import java.util.List;
 @Primary
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ClientMapper {
-    @Mapping(target = "address", expression = "java(com.branches.mapper.ClientMapperImpl.toAddressGetResponse())")
-    @Mapping(target = "phones", expression = "java(com.branches.mapper.ClientMapperImpl.toPhoneGetResponseList())")
     List<ClientGetResponse> toClientGetResponseList(List<Client> clientList);
 }
