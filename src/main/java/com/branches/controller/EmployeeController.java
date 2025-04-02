435060a227da4c12812b797e438a2b3d333bde84
Branches <marcus.branches@icloud.com>
2025-04-02 19:45:26 -0300

    feat: findAll criado para request get

--- src/main/java/com/branches/controller/EmployeeController.java
+++ src/main/java/com/branches/controller/EmployeeController.java
@@ -1,12 +1,31 @@
 package com.branches.controller;
 
+import com.branches.mapper.EmployeeMapper;
+import com.branches.model.Employee;
+import com.branches.response.EmployeeGetResponse;
+import com.branches.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
+    private final EmployeeMapper mapper;
+    private final EmployeeService service;
 
+    @GetMapping
+    public ResponseEntity<List<EmployeeGetResponse>> findAll(@RequestParam(required = false) String firstName) {
+        List<Employee> employees = service.findAll(firstName);
+
+        List<EmployeeGetResponse> response = mapper.toEmployeeGetResponseList(employees);
+
+        return ResponseEntity.ok(response);
+    }
 }
