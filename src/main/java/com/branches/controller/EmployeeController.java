21a6870cfd1a3363f4801f4bcf298bd17954b61a
Branches <marcus.branches@icloud.com>
2025-04-02 19:42:57 -0300

    feat: employee controller criado

---
+++ src/main/java/com/branches/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.branches.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+}
