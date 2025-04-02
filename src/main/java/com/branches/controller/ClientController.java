b2b3284499b31527e1f281796a5b6b0c35270564
Branches <marcus.branches@icloud.com>
2025-04-02 18:57:02 -0300

    feat: método findAll adicionado ao request get

--- src/main/java/com/branches/controller/ClientController.java
+++ src/main/java/com/branches/controller/ClientController.java
@@ -1,12 +1,32 @@
 package com.branches.controller;
 
+import com.branches.mapper.ClientMapper;
+import com.branches.model.Client;
+import com.branches.response.ClientGetResponse;
+import com.branches.service.ClientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RequestMapping("v1/clients")
 @RestController
 @RequiredArgsConstructor
 public class ClientController {
+    private final ClientService service;
+    private final ClientMapper mapper;
 
+    @GetMapping
+    public ResponseEntity<List<ClientGetResponse>> findAll(@RequestParam(required = false) String firstName) {
+        List<Client> clients = service.findAll(firstName);
+
+        List<ClientGetResponse> response = mapper.toClientGetResponseList(clients);
+
+        return ResponseEntity.ok(response);
+    }
 }
