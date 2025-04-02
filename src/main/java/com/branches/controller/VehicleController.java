9f2cbb20e3ecc5466bdfb4c7f1b1ad7cfb05ad54
Branches <marcus.branches@icloud.com>
2025-04-02 20:01:50 -0300

    feat: método findAll criado para request get

--- src/main/java/com/branches/controller/VehicleController.java
+++ src/main/java/com/branches/controller/VehicleController.java
@@ -1,13 +1,30 @@
 package com.branches.controller;
 
+import com.branches.mapper.VehicleMapper;
+import com.branches.model.Vehicle;
+import com.branches.response.VehicleGetResponse;
 import com.branches.service.VehicleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/vehicles")
 @RequiredArgsConstructor
 public class VehicleController {
     private final VehicleService service;
+    private final VehicleMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<List<VehicleGetResponse>> findAll() {
+        List<Vehicle> vehicles = service.findAll();
+
+        List<VehicleGetResponse> response = mapper.toVehicleGetResponseList(vehicles);
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/branches/service/VehicleService.java
+++ src/main/java/com/branches/service/VehicleService.java
@@ -1,12 +1,18 @@
 package com.branches.service;
 
+import com.branches.model.Vehicle;
 import com.branches.repository.VehicleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class VehicleService {
     private final VehicleRepository repository;
 
+    public List<Vehicle> findAll() {
+        return repository.findAll();
+    }
 }
