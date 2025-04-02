9e8f340be415c7ed769789f40390302baf72940e
Branches <marcus.branches@icloud.com>
2025-04-02 18:19:14 -0300

    feat: service e repository criado para client

---
+++ src/main/java/com/branches/service/ClientService.java
@@ -0,0 +1,17 @@
+package com.branches.service;
+
+import com.branches.model.Client;
+import com.branches.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClientService {
+    private final ClientRepository repository;
+    public List<Client> findAll(String firstName) {
+        return firstName == null ? repository.findAll() : repository.findAllByName(firstName);
+    }
+}
