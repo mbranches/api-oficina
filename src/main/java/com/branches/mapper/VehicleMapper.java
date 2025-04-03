168e3139a0dfe1a0e347ad779316d97aab9d2c97
Branches <marcus.branches@icloud.com>
2025-04-03 13:02:09 -0300

    feat: mappers utilizado no request post adicionados

--- src/main/java/com/branches/mapper/VehicleMapper.java
+++ src/main/java/com/branches/mapper/VehicleMapper.java
@@ -1,8 +1,11 @@
 package com.branches.mapper;
 
 import com.branches.model.Vehicle;
+import com.branches.request.VehiclePostRequest;
 import com.branches.response.VehicleGetResponse;
+import com.branches.response.VehiclePostResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.springframework.context.annotation.Primary;
 
@@ -12,4 +15,10 @@ import java.util.List;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface VehicleMapper {
     List<VehicleGetResponse> toVehicleGetResponseList(List<Vehicle> vehicleList);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "client", ignore = true)
+    Vehicle toVehicle(VehiclePostRequest postRequest);
+
+    VehiclePostResponse toVehiclePostResponse(Vehicle response);
 }
