d2479209a0203be6c59123ff1fe40660db27a06a
Branches <marcus.branches@icloud.com>
2025-04-03 12:50:18 -0300

    feat: método save criado para persistir funcionario

--- src/main/java/com/branches/controller/EmployeeController.java
+++ src/main/java/com/branches/controller/EmployeeController.java
@@ -1,13 +1,13 @@
 package com.branches.controller;
 
+import com.branches.request.EmployeePostRequest;
 import com.branches.response.EmployeeGetResponse;
+import com.branches.response.EmployeePostResponse;
 import com.branches.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,12 @@ public class EmployeeController {
         List<EmployeeGetResponse> response = service.findAll(firstName);;
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeePostResponse> save(@RequestBody EmployeePostRequest postRequest) {
+        EmployeePostResponse response = service.save(postRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/branches/service/EmployeeService.java
+++ src/main/java/com/branches/service/EmployeeService.java
@@ -1,22 +1,50 @@
 package com.branches.service;
 
 import com.branches.mapper.EmployeeMapper;
+import com.branches.model.Address;
+import com.branches.model.Category;
 import com.branches.model.Employee;
 import com.branches.repository.EmployeeRepository;
+import com.branches.request.EmployeePostRequest;
 import com.branches.response.EmployeeGetResponse;
+import com.branches.response.EmployeePostResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
     private final EmployeeRepository repository;
+    private final EmployeeMapper mapper;
+    private final CategoryService categoryService;
+    private final AddressService addressService;
 
     public List<EmployeeGetResponse> findAll(String firstName) {
         List<Employee> response = firstName == null ? repository.findAll() : repository.findByNameContaining(firstName);
 
         return mapper.toEmployeeGetResponseList(response);
+    }
+
+    public EmployeePostResponse save(EmployeePostRequest postRequest) {
+        Category category = categoryService.findByIdOrElseThrowsNotFoundException(postRequest.getCategoryId());
+
+        Employee employeeToSave = mapper.toEmployee(postRequest);
+        employeeToSave.setCategory(category);
+
+        Address address = employeeToSave.getAddress();
+
+        Optional<Address> addressSearched = addressService.findAddress(address);
+        Address addressSaved = addressSearched.orElseGet(() -> addressService.save(address));
+        employeeToSave.setAddress(addressSaved);
+
+        employeeToSave.getPhones()
+                .forEach(phone -> phone.setEmployee(employeeToSave));
+
+        Employee response = repository.save(employeeToSave);
+
+        return mapper.toEmployeePostResponse(response);
     }
 }
