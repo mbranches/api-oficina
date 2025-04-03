616b0d4b1092ebff7fb8280e0817267b926e7bb8
Branches <marcus.branches@icloud.com>
2025-04-03 12:49:44 -0300

    feat: mappers utilizados no post adicionados

--- src/main/java/com/branches/mapper/EmployeeMapper.java
+++ src/main/java/com/branches/mapper/EmployeeMapper.java
@@ -1,8 +1,11 @@
 package com.branches.mapper;
 
 import com.branches.model.Employee;
+import com.branches.request.EmployeePostRequest;
 import com.branches.response.EmployeeGetResponse;
+import com.branches.response.EmployeePostResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.springframework.context.annotation.Primary;
 
@@ -12,4 +15,10 @@ import java.util.List;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface EmployeeMapper {
     List<EmployeeGetResponse> toEmployeeGetResponseList(List<Employee> employeeList);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    Employee toEmployee(EmployeePostRequest postRequest);
+
+    EmployeePostResponse toEmployeePostResponse(Employee response);
 }
