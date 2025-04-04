3ce93077784e25b05b1b984581b7010c00e8afea
Branches <marcus.branches@icloud.com>
2025-04-04 18:45:48 -0300

    refactor: métodos de CategoryUtils postos como estáticos

--- src/test/java/com/branches/utils/CategoryUtils.java
+++ src/test/java/com/branches/utils/CategoryUtils.java
@@ -6,9 +6,8 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
 public class CategoryUtils {
-    public List<Category> newCategoryList() {
+    public static List<Category> newCategoryList() {
         Category mecanico = Category.builder().id(1L).name("Mecânico").hourlyPrice(50.0).build();
         Category borracheiro = Category.builder().id(2L).name("Borracheiro").hourlyPrice(40.0).build();
         Category pintor = Category.builder().id(3L).name("Pintor").hourlyPrice(30.0).build();
