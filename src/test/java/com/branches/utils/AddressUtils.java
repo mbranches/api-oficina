97681a1925b07a4b3eb0e291b66c9fd5764edd0f
Branches <marcus.branches@icloud.com>
2025-04-04 19:08:10 -0300

    refactor: métodos de AddressUtils tornados estáticos

--- src/test/java/com/branches/utils/AddressUtils.java
+++ src/test/java/com/branches/utils/AddressUtils.java
@@ -6,9 +6,8 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
 public class AddressUtils {
-    public List<Address> newAddressList() {
+    public static List<Address> newAddressList() {
         Address address1 = Address.builder().id(1L).street("Magalhães Barata").district("Nazaré").city("Belém").state("Pa").build();
         Address address2 = Address.builder().id(2L).street("Gentil").district("Nazaré").city("Belém").state("Pa").build();
         Address address3 = Address.builder().id(2L).street("Br-316").district("Centro").city("Ananindeua").state("Pa").build();
@@ -16,7 +15,7 @@ public class AddressUtils {
         return new ArrayList<>(List.of(address1, address2, address3));
     }
 
-    public Address newAddressToSave() {
+    public static Address newAddressToSave() {
         return Address.builder()
                 .id(4L)
                 .street("Almirante Barroso")
