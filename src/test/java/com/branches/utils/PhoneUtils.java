cc3fe65d7e2653cfb6b856b7e97b150ac403cacf
Branches <marcus.branches@icloud.com>
2025-04-04 19:27:38 -0300

    feat: método de criar telefone criado em uma classe util

---
+++ src/test/java/com/branches/utils/PhoneUtils.java
@@ -0,0 +1,10 @@
+package com.branches.utils;
+
+import com.branches.model.Phone;
+import com.branches.model.PhoneType;
+
+public class PhoneUtils {
+    public static Phone newPhone(Long id) {
+        Phone.builder().id(id).number("21121521").phoneType(PhoneType.celular).build();
+    }
+}
