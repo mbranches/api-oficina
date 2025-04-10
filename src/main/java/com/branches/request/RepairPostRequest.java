498ba83bcc24970a78464f341f8e1babcefe4c76
Branches <marcus.branches@icloud.com>
2025-04-10 12:56:12 -0300

    feat: adiciona beans validation ao postRequest

--- src/main/java/com/branches/request/RepairPostRequest.java
+++ src/main/java/com/branches/request/RepairPostRequest.java
@@ -1,5 +1,7 @@
 package com.branches.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,9 +11,12 @@ import java.util.List;
 @Data
 @Builder
 public class RepairPostRequest {
+    @NotNull(message = "The field 'clientId' cannot be null")
     private Long clientId;
+    @NotNull(message = "The field 'vehicleId' cannot be null")
     private Long vehicleId;
     private List<RepairPieceByRepairPostRequest> pieces;
     private List<RepairEmployeeByRepairPostRequest> employees;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate endDate;
 }
