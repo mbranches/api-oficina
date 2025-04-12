28ac557e251f7b771d7f9aa4298a65a6378dc22d
Branches <marcus.branches@icloud.com>
2025-04-12 15:16:20 -0300

    test: adiciona testes para findAllByRepair

--- src/test/java/com/branches/service/RepairEmployeeServiceTest.java
+++ src/test/java/com/branches/service/RepairEmployeeServiceTest.java
@@ -1,8 +1,10 @@
 package com.branches.service;
 
+import com.branches.model.Repair;
 import com.branches.model.RepairEmployee;
 import com.branches.repository.RepairEmployeeRepository;
 import com.branches.utils.RepairEmployeeUtils;
+import com.branches.utils.RepairUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,5 +40,37 @@ class RepairEmployeeServiceTest {
                 .isNotNull()
                 .isNotEmpty()
                 .containsExactlyElementsOf(repairEmployeesToSave);
+    }
+
+    @Test
+    @DisplayName("findAllByRepair returns all repair employees from given repair when successful")
+    @Order(2)
+    void findAllByRepair_ReturnsAllRepairEmployeesFromGiveRepair_WhenSuccessful() {
+        Repair repairToSearch = RepairUtils.newRepairList().getFirst();
+        List<RepairEmployee> expectedResponse = List.of(RepairEmployeeUtils.newRepairEmployeeSaved());
+
+        BDDMockito.when(repository.findAllByRepair(repairToSearch)).thenReturn(expectedResponse);
+
+        List<RepairEmployee> response = service.findAllByRepair(repairToSearch);
+
+        Assertions.assertThat(response)
+                .isNotNull()
+                .isNotEmpty()
+                .containsExactlyElementsOf(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("findAllByRepair returns an empty list when when given repair contain no employees")
+    @Order(3)
+    void findAllByRepair_ReturnsEmptyList_WhenGivenRepairDoesNotContainEmployees() {
+        Repair repairToSearch = RepairUtils.newRepairList().getLast();
+
+        BDDMockito.when(repository.findAllByRepair(repairToSearch)).thenReturn(Collections.emptyList());
+
+        List<RepairEmployee> response = service.findAllByRepair(repairToSearch);
+
+        Assertions.assertThat(response)
+                .isNotNull()
+                .isEmpty();
     }
 }
--- src/test/java/com/branches/utils/RepairEmployeeUtils.java
+++ src/test/java/com/branches/utils/RepairEmployeeUtils.java
@@ -3,11 +3,27 @@ package com.branches.utils;
 import com.branches.model.Category;
 import com.branches.model.Employee;
 import com.branches.model.RepairEmployee;
+import com.branches.model.RepairEmployeeKey;
 import com.branches.request.RepairEmployeeByRepairPostRequest;
 import com.branches.response.EmployeeByRepairResponse;
 import com.branches.response.RepairEmployeeByRepairResponse;
 
 public class RepairEmployeeUtils {
+    public static RepairEmployee newRepairEmployeeSaved() {
+        RepairEmployeeKey key = new RepairEmployeeKey(1L, 1L);
+
+        Employee employee = EmployeeUtils.newEmployeeList().getFirst();
+        int hoursWorked = 1;
+
+        return RepairEmployee.builder()
+                .id(key)
+                .repair(RepairUtils.newRepairList().getFirst())
+                .employee(employee)
+                .hoursWorked(hoursWorked)
+                .totalValue(employee.getCategory().getHourlyPrice() * hoursWorked)
+                .build();
+    }
+
     public static RepairEmployeeByRepairPostRequest newRepairEmployeePostRequest() {
         return RepairEmployeeByRepairPostRequest.builder().employeeId(4L).hoursWorked(5).build();
     }
